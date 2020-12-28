1020bd7f7d136b23cb325ad0c9a8f7d38e01d468
yossiee <38056766+yossiee@users.noreply.github.com>
2020-12-29 08:01:41 +0900

    Add: profile image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,8 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
+import ProfileImg from "../images/profile.png"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -11,8 +12,8 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div style={{ maxWidth: `100px`, marginBottom: `1.45rem` }}>
+      <img src={ProfileImg} alt="プロフィール画像" />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
