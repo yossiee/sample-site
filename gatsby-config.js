5f24108984938b06e4cfe4dd5074238ac70f6871
yossiee <38056766+yossiee@users.noreply.github.com>
2020-12-31 14:09:01 +0900

    Fix: typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsyby-transformer-remark`
+      resolve: `gatsby-transformer-remark`
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
