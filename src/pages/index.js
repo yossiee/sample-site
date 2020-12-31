bbe0f746b0817d566276b9ef2d33b3945d8dbc11
yossiee <38056766+yossiee@users.noreply.github.com>
2020-12-31 14:59:10 +0900

    Fix: sample text

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>やっほー、こんにちは</h1>
+    <p>サンプルサイトへようこそ。</p>
+    <p>当サイトは、Gatsby を使って構築してみました。</p>
     <div style={{ maxWidth: `100px`, marginBottom: `1.45rem` }}>
       <img src={ProfileImg} alt="プロフィール画像" />
     </div>
+    <Link to="/entry/">エントリ一覧</Link> <br />
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
